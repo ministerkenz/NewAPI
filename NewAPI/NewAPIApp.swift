e051a377a5a8d522194bfc0431426c996abc9303
Kenzo Yubitani <kommoken@gmail.com>
2024-12-10 14:35:49 -0500

    Initial Commit

---
+++ NewAPI/NewAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAPIApp.swift
+//  NewAPI
+//
+//  Created by Kenzo Yubitani (student LM) on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
